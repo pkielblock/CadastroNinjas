b672c1f9905a70e7b06b482964ca2558e92a0045
Pedro Kielblock <pedro@kielblock.dev>
2025-01-31 17:48:54 -0300

    feat: extend JpaRepository in NinjaRepository

--- src/main/java/dev/kielblock/cadastroninjas/Ninjas/NinjaRepository.java
+++ src/main/java/dev/kielblock/cadastroninjas/Ninjas/NinjaRepository.java
@@ -1,4 +1,9 @@
 package dev.kielblock.cadastroninjas.Ninjas;
 
-public interface NinjaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel, UUID> {
+
 }
